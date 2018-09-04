951ca2dd86490dbf58ecf543001c8c43d8775fff
Jeffrey Su <zsu@senparc.com>
2018-09-04 10:54:19 +0800

    补充所有Request类型接口

---
+++ src/Senparc.NeuChar/Entities/Base/Request/Interfaces/IRequestMessageVideo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.NeuChar.Entities
+{
+    /// <summary>
+    /// 所有RequestMessageVideo的接口
+    /// </summary>
+    public interface IRequestMessageVideo : IRequestMessageBase
+    {
+    }
+}
---
+++ src/Senparc.NeuChar/Entities/Base/Request/Interfaces/IRequestMessageVoice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.NeuChar.Entities
+{
+    /// <summary>
+    /// 所有RequestMessageVoice的接口
+    /// </summary>
+    public interface IRequestMessageVoice : IRequestMessageBase
+    {
+      
+    }
+}
