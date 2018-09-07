b388f4be4882326c8c9e7df11b50e6aec7e8a18f
Jeffrey Su <zsu@senparc.com>
2018-09-07 21:37:33 +0800

    添加单元测试项目

---
+++ src/Senparc.NeuChar.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Senparc.NeuChar.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
