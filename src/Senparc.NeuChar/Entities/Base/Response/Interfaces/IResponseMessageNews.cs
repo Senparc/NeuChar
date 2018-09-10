773ab05baf69b35adbd4c46120f37756f140c103
Jeffrey Su <zsu@senparc.com>
2018-09-10 22:48:02 +0800

    v0.0.8 添加News响应消息接口及处理

--- src/Senparc.NeuChar/Entities/Base/Response/Interfaces/IResponseMessageNews.cs
+++ src/Senparc.NeuChar/Entities/Base/Response/Interfaces/IResponseMessageNews.cs
@@ -10,6 +10,14 @@ namespace Senparc.NeuChar.Entities
     /// </summary>
     public interface IResponseMessageNews : IResponseMessageBase
     {
+        /// <summary>
+        /// 文章数量
+        /// </summary>
+        int ArticleCount { get; set; }//这里开放set只为了逆向从Response的Xml转成实体的操作一致性，没有实际意义。
 
+        /// <summary>
+        /// 文章列表，微信客户端只能输出前10条（可能未来数字会有变化，出于视觉效果考虑，建议控制在8条以内）
+        /// </summary>
+        List<Article> Articles { get; set; }
     }
 }
