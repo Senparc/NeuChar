6de3d2eee5e2c873f7e5def6e1298962a9068f96
Jeffrey Su <zsu@senparc.com>
2018-09-10 21:39:01 +0800

    v0.0.7.5 更多NeuChar及NeuralNode相关方法合并到MessageHandler基类中。

--- src/Senparc.NeuChar/MessageHandlers/MessageHandler.cs
+++ src/Senparc.NeuChar/MessageHandlers/MessageHandler.cs
@@ -50,8 +50,12 @@ Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
 
 using System;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using Senparc.CO2NET.Extensions;
+using Senparc.CO2NET.Helpers;
+using Senparc.CO2NET.Trace;
 using Senparc.CO2NET.Utilities;
 using Senparc.NeuChar.ApiHandlers;
 using Senparc.NeuChar.Context;
@@ -235,6 +239,12 @@ namespace Senparc.NeuChar.MessageHandlers
             }
         }
 
+        static MessageHandler()
+        {
+            //注册节点类型
+            Senparc.NeuChar.Register.RegisterNeuralNode("MessageHandlerNode", typeof(MessageHandlerNode));
+        }
+
         /// <summary>
         /// 构造函数公用的初始化方法
         /// </summary>
@@ -323,6 +333,104 @@ namespace Senparc.NeuChar.MessageHandlers
         public virtual void OnExecuted()
         {
         }
+
+        #region NeuChar 方法
+
+        /// <summary>
+        /// NeuChar 请求
+        /// </summary>
+        public virtual IResponseMessageBase OnNeuCharRequest(RequestMessageNeuChar requestMessage)
+        {
+            try
+            {
+                var path = ServerUtility.ContentRootMapPath("~/App_Data/NeuChar");
+                var file = Path.Combine(path, "NeuCharRoot.config");
+                string result = null;
+
+                switch (requestMessage.NeuCharMessageType)
+                {
+                    case NeuCharActionType.GetConfig:
+                        {
+                            if (File.Exists(file))
+                            {
+                                using (var fs = FileHelper.GetFileStream(file))
+                                {
+                                    using (var sr = new StreamReader(fs, Encoding.UTF8))
+                                    {
+                                        var json = sr.ReadToEnd();
+                                        result = json;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                result = "{}";//TODO:初始化一个对象
+                            }
+                        }
+                        break;
+                    case NeuCharActionType.SaveConfig:
+                        {
+                            var configRootJson = requestMessage.ConfigRoot;
+                            SenparcTrace.SendCustomLog("收到NeuCharRequest", configRootJson);
+                            var configRoot = SerializerHelper.GetObject<ConfigRoot>(configRootJson);//这里只做序列化校验
+
+                            //TODO:进行验证
+
+
+                            if (!Directory.Exists(path))
+                            {
+                                Directory.CreateDirectory(path);
+                            }
+
+                            var fileBak = Path.Combine(path, "NeuCharRoot.bak.config");
+                            //TODO：后期也可以考虑把不同模块分离到不同的文件中
+
+                            File.Delete(fileBak);
+
+                            using (var fs = new FileStream(fileBak, FileMode.CreateNew))
+                            {
+                                using (var sw = new StreamWriter(fs))
+                                {
+                                    sw.Write(configRootJson);
+                                    sw.Flush();
+                                }
+                            }
+
+                            //替换备份文件
+                            File.Delete(file);
+                            File.Move(fileBak, file);
+
+                            //刷新数据
+                            var neuralSystem = NeuralSystem.Instance;
+                            neuralSystem.ReloadNode();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+
+                var successMsg = new
+                {
+                    success = true,
+                    result = result
+                };
+                TextResponseMessage = successMsg.ToJson();
+            }
+            catch (Exception ex)
+            {
+                var errMsg = new
+                {
+                    success = false,
+                    result = ex.Message
+                };
+                TextResponseMessage = errMsg.ToJson();
+            }
+
+            return null;
+        }
+
+        #endregion
 
 
         ///// <summary>
