93e5cb8c5f1ff8ba3a05194d48c8367960a1624c
Jeffrey Su <zsu@senparc.com>
2018-09-14 00:22:13 +0800

    v0.0.9 添加ApiBindAttributes

---
+++ src/Senparc.NeuChar/ApiBindAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.NeuChar
+{
+    /// <summary>
+    /// 自动绑定属性
+    /// </summary>
+    public class ApiBindAttributes
+    {
+        /// <summary>
+        /// 平台类型
+        /// </summary>
+        public PlatformType PlatformType { get; set; } = PlatformType.General;
+
+        /// <summary>
+        /// 平台内唯一名称（如使用 PlatformType.General，请使用宇宙唯一名称）
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// ApiBindAttributes 构造函数
+        /// </summary>
+        public ApiBindAttributes() { }
+
+        /// <summary>
+        /// ApiBindAttributes 构造函数
+        /// </summary>
+        /// <param name="platformType">平台类型</param>
+        /// <param name="name">平台内唯一名称（如使用 PlatformType.General，请使用宇宙唯一名称）</param>
+        public ApiBindAttributes(PlatformType platformType, string name)
+        {
+            PlatformType = platformType;
+            Name = name;
+        }
+    }
+}
