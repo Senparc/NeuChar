21266ed3e1f480b6dc6bffcf5467721174f70740
Jeffrey Su <zsu@senparc.com>
2018-09-16 18:57:17 +0800

    添加单元测试

---
+++ src/Senparc.NeuChar.Tests/BaseTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Senparc.CO2NET;
+using Senparc.CO2NET.RegisterServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.NeuChar.Tests
+{
+    public class BaseTest
+    {
+        protected static IRegisterService registerService;
+
+        public BaseTest()
+        {
+            RegisterServiceCollection();
+
+            RegisterServiceStart();
+        }
+
+        /// <summary>
+        /// 注册 IServiceCollection 和 MemoryCache
+        /// </summary>
+        public static void RegisterServiceCollection()
+        {
+            var serviceCollection = new ServiceCollection();
+            var configBuilder = new ConfigurationBuilder();
+            var config = configBuilder.Build();
+            serviceCollection.AddSenparcGlobalServices(config);
+            serviceCollection.AddMemoryCache();//使用内存缓存
+        }
+
+        /// <summary>
+        /// 注册 RegisterService.Start()
+        /// </summary>
+        public static void RegisterServiceStart(bool autoScanExtensionCacheStrategies = false)
+        {
+            //注册
+            var mockEnv = new Mock<IHostingEnvironment>();
+            mockEnv.Setup(z => z.ContentRootPath).Returns(() => UnitTestHelper.RootPath);
+            registerService = RegisterService.Start(mockEnv.Object, new SenparcSetting() { IsDebug = true })
+                .UseSenparcGlobal(autoScanExtensionCacheStrategies);
+        }
+    }
+}
---
+++ src/Senparc.NeuChar.Tests/RegisterTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Senparc.CO2NET.RegisterServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.NeuChar.Tests
+{
+    public class ApiTest: BaseTest
+    {
+
+        public void MethodForTest(string accessTokenOrApi,string p1,string p2)
+        {
+            Console.WriteLine($"accessTokenOrApi:{accessTokenOrApi} , p1:{p1} , p2 {p2}");
+        }
+    }
+
+    [TestClass]
+    public class RegisterTests
+    {
+        [TestMethod]
+        public void RegisterApiBindTest()
+        {
+            Register.RegisterApiBind();
+
+            Assert.IsTrue(Register.ApiBindInfoCollection.Count > 0);
+        }
+
+    }
+}
