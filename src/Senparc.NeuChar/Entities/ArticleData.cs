26aae8aa9930f5e859224dc310ce2835c4a3c482
Jeffrey Su <zsu@senparc.com>
2018-09-29 18:13:33 +0800

    v0.2.2 添加 ArticleData.cs

---
+++ src/Senparc.NeuChar/Entities/ArticleData.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.NeuChar.Entities
+{
+    /// <summary>
+    /// 图文数据
+    /// </summary>
+    public class ArticleData
+    {
+        /// <summary>
+        /// 封面图地址
+        /// </summary>
+        public string ThumbCoverUrl
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 作者
+        /// </summary>
+        public string Author
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Content
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 摘要
+        /// </summary>
+        public string Digest
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 原地址
+        /// </summary>
+        public string ContentSourceUrl
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 多图文
+        /// </summary>
+        public List<string> ArticleIds
+        {
+            get;
+            set;
+        }
+
+        public ArticleData()
+        {
+            ArticleIds = new List<string>();
+        }
+    }
+}
