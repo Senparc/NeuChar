ec7610d29c80d3ac98781db3e24dddc405e293bd
Jeffrey Su <zsu@senparc.com>
2018-10-20 01:07:49 +0800

    添加注释

--- src/Senparc.NeuChar/DataEngine.cs
+++ src/Senparc.NeuChar/DataEngine.cs
@@ -11,17 +11,23 @@ namespace Senparc.NeuChar
     /// </summary>
     public class DataEngine
     {
+        /// <summary>
+        /// 文件路径（相对路径）
+        /// </summary>
         public string FullFilePath { get; set; }
 
+        /// <summary>
+        /// NeuralSystem 单例对象
+        /// </summary>
         public NeuralSystem NeuralSystem { get { return NeuralSystem.Instance; } }
 
         /// <summary>
         /// 数据处理引擎 构造函数
         /// </summary>
-        /// <param name="file">文件相对路径</param>
+        /// <param name="filePath">文件相对路径</param>
         public DataEngine(string filePath = "~/App_Data/NeuChar")
         {
-            FullFilePath = filePath;
+              FullFilePath = filePath;
 
 
 
