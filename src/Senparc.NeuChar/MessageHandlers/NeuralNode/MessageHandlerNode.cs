e16d1aefea1434c8a9040b3fa779c13ebdf7e292
Jeffrey Su <zsu@senparc.com>
2018-10-24 00:39:00 +0800

    modify

--- src/Senparc.NeuChar/MessageHandlers/NeuralNode/MessageHandlerNode.cs
+++ src/Senparc.NeuChar/MessageHandlers/NeuralNode/MessageHandlerNode.cs
@@ -159,10 +159,10 @@ namespace Senparc.NeuChar.NeuralSystems
 
             if (responseMessage != null)
             {
-                if (messageHandler.MessageEntityEnlightener.PlatformType == PlatformType.WeChat_MiniProgram && requestMessage is IResponseMessageText)
+                if (messageHandler.MessageEntityEnlightener.PlatformType == PlatformType.WeChat_MiniProgram && responseMessage is IResponseMessageText)
                 {
                     //小程序
-                    messageHandler.ApiEnlightener.SendText(accessTokenOrApi, messageHandler.WeixinOpenId, (requestMessage as IResponseMessageText).Content);
+                    messageHandler.ApiEnlightener.SendText(accessTokenOrApi, messageHandler.WeixinOpenId, (responseMessage as IResponseMessageText).Content);
                     return new SuccessResponseMessage();
                 }
                 else
