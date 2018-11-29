bb5694eed80d333fa2924e262d4dec301ec1e7e8
Jeffrey Su <zsu@senparc.com>
2018-11-29 17:55:17 +0800

    更新 NeuCharAppMessageEntityEnlightener

---
+++ src/Senparc.NeuChar.App/MessageHandlers/NeuCharAppMessageEntityEnlightener.cs
@@ -0,0 +1,110 @@
+﻿using Senparc.NeuChar.Entities;
+using Senparc.NeuChar.Exceptions;
+using Senparc.NeuChar.MessageHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.NeuChar.App.MessageHandlers
+{
+    /// <summary>
+    /// NeuChar-App MessageEntityEnlightener
+    /// </summary>
+    public class NeuCharAppMessageEntityEnlightener : MessageEntityEnlightener
+    {
+        //说明：此处不需要强制使用单例模式
+
+        /// <summary>
+        /// MpMessageEntityEnlightener 全局对象
+        /// </summary>
+        public static NeuCharAppMessageEntityEnlightener Instance = new NeuCharAppMessageEntityEnlightener(NeuChar.PlatformType.General);
+
+        NeuCharAppMessageEntityEnlightener(NeuChar.PlatformType platformType) : base(platformType) { }
+
+        public override IRequestMessageEvent NewRequestMessageEvent()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IRequestMessageEvent 响应类型");
+        }
+
+        public override IRequestMessageFile NewRequestMessageFile()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IRequestMessageFile 响应类型");
+        }
+
+        public override IRequestMessageImage NewRequestMessageImage()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IRequestMessageImage 响应类型");
+        }
+
+        public override IRequestMessageLink NewRequestMessageLink()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IRequestMessageLink 响应类型");
+        }
+
+        public override IRequestMessageLocation NewRequestMessageLocation()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IRequestMessageLocation 响应类型");
+        }
+
+        public override IRequestMessageShortVideo NewRequestMessageShortVideo()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IRequestMessageShortVideo 响应类型");
+        }
+
+        public override IRequestMessageText NewRequestMessageText()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IRequestMessageText 响应类型");
+        }
+
+        public override IRequestMessageVideo NewRequestMessageVideo()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IRequestMessageVideo 响应类型");
+        }
+
+        public override IRequestMessageVoice NewRequestMessageVoice()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IRequestMessageVoice 响应类型");
+        }
+
+        public override IResponseMessageImage NewResponseMessageImage()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IResponseMessageImage 响应类型");
+        }
+
+        public override IResponseMessageMpNews NewResponseMessageMpNews()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IResponseMessageMpNews 响应类型");
+        }
+
+        public override IResponseMessageMusic NewResponseMessageMusic()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IResponseMessageMusic 响应类型");
+        }
+
+        public override IResponseMessageNews NewResponseMessageNews()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IResponseMessageNews 响应类型");
+        }
+
+        public override IResponseMessageText NewResponseMessageText()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IResponseMessageText 响应类型");
+        }
+
+        public override IResponseMessageTransfer_Customer_Service NewResponseMessageTransfer_Customer_Service()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IResponseMessageTransfer_Customer_Service 响应类型");
+        }
+
+        public override IResponseMessageVideo NewResponseMessageVideo()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IResponseMessageVideo 响应类型");
+        }
+
+        public override IResponseMessageVoice NewResponseMessageVoice()
+        {
+            throw new MessageHandlerException("NeuChar-App SDK 不支持 IResponseMessageVoice 响应类型");
+        }
+    }
+}
--- src/Senparc.NeuChar.App/MessageHandlers/NeuCharAppMessageHandler.cs
+++ src/Senparc.NeuChar.App/MessageHandlers/NeuCharAppMessageHandler.cs
@@ -1,6 +1,9 @@
-﻿using Senparc.NeuChar.ApiHandlers;
+﻿using Senparc.CO2NET.Helpers;
+using Senparc.NeuChar.ApiHandlers;
 using Senparc.NeuChar.Context;
 using Senparc.NeuChar.Entities;
+using Senparc.NeuChar.Exceptions;
+using Senparc.NeuChar.Helpers;
 using Senparc.NeuChar.MessageHandlers;
 using System;
 using System.IO;
@@ -14,30 +17,132 @@ namespace Senparc.NeuChar.App.MessageHandlers
     /// </summary>
     public class NeuCharAppMessageHandler : MessageHandler<NeuCharAppMessageContext, RequestMessageNeuChar, SuccessResponseMessage>
     {
-        public NeuCharAppMessageHandler(Stream inputStream,EncryptPostModel postModel)
-            :base(inputStream, postModel)
+
+        #region 私有方法
+
+        /// <summary>
+        /// 标记为已重复消息
+        /// </summary>
+        private void MarkRepeatedMessage()
+        {
+            CancelExcute = true;//重复消息，取消执行
+            MessageIsRepeated = true;
+        }
+
+        #endregion
+
+
+        public NeuCharAppMessageHandler(Stream inputStream, EncryptPostModel postModel)
+            : base(inputStream, postModel)
         {
 
         }
 
         public override GlobalMessageContext<NeuCharAppMessageContext, RequestMessageNeuChar, SuccessResponseMessage> GlobalMessageContext => throw new NotImplementedException();
 
-        public override MessageEntityEnlightener MessageEntityEnlightener => throw new NotImplementedException();
+        public override MessageEntityEnlightener MessageEntityEnlightener => NeuCharAppMessageEntityEnlightener.Instance;
 
         public override ApiEnlightener ApiEnlightener => throw new NotImplementedException();
 
-        public override XDocument ResponseDocument => throw new NotImplementedException();
+        public override XDocument ResponseDocument => ResponseMessage != null ? NeuChar.Helpers.EntityHelper.ConvertEntityToXml(ResponseMessage as ResponseMessageBase) : null;
 
-        public override XDocument FinalResponseDocument => throw new NotImplementedException();
+
+        public override XDocument FinalResponseDocument => null;
 
         public override void BuildResponseMessage()
         {
-            throw new NotImplementedException();
+            //不做处理
         }
 
-        public override XDocument Init(XDocument requestDocument, IEncryptPostModel postModel)
+        public override XDocument Init(XDocument postDataDocument, IEncryptPostModel postModel)
         {
-            throw new NotImplementedException();
+            //进行加密判断并处理
+            var postDataStr = postDataDocument.ToString();
+
+            XDocument decryptDoc = postDataDocument;
+
+            //if (postModel != null && postDataDocument.Root.Element("Encrypt") != null && !string.IsNullOrEmpty(postDataDocument.Root.Element("Encrypt").Value))
+            //{
+            //    //使用了加密
+            //    UsingEcryptMessage = true;
+            //    EcryptRequestDocument = postDataDocument;
+
+            //    WXBizMsgCrypt msgCrype = new WXBizMsgCrypt(_postModel.Token, _postModel.EncodingAESKey, _postModel.AppId);
+            //    string msgXml = null;
+            //    var result = msgCrype.DecryptMsg(_postModel.Msg_Signature, _postModel.Timestamp, _postModel.Nonce, postDataStr, ref msgXml);
+
+            //    //判断result类型
+            //    if (result != 0)
+            //    {
+            //        //验证没有通过，取消执行
+            //        CancelExcute = true;
+            //        return null;
+            //    }
+
+            //    if (postDataDocument.Root.Element("FromUserName") != null && !string.IsNullOrEmpty(postDataDocument.Root.Element("FromUserName").Value))
+            //    {
+            //        //TODO：使用了兼容模式，进行验证即可
+            //        UsingCompatibilityModelEcryptMessage = true;
+            //    }
+
+            //    decryptDoc = XDocument.Parse(msgXml);//完成解密
+            //}
+
+            var msgType = MsgTypeHelper.GetRequestMsgType(decryptDoc);
+            if (msgType!= RequestMsgType.NeuChar)
+            {
+                throw new MessageHandlerException("仅支持 NeuChar 类型请求");
+            }
+
+            RequestMessage = new RequestMessageNeuChar();
+            if (UsingEcryptMessage)
+            {
+                RequestMessage.Encrypt = postDataDocument.Root.Element("Encrypt").Value;
+            }
+
+
+            //TODO:分布式系统中本地的上下文会有同步问题，需要同步使用远程的储存
+            if (MessageContextGlobalConfig.UseMessageContext)
+            {
+                //var omit = OmitRepeatedMessageFunc == null || OmitRepeatedMessageFunc(RequestMessage);
+
+                lock (MessageContextGlobalConfig.OmitRepeatLock)//TODO:使用分布式锁
+                {
+                    #region 消息去重
+
+                    if (/*omit &&*/
+                        OmitRepeatedMessage &&
+                        CurrentMessageContext.RequestMessages.Count > 0
+                         //&& !(RequestMessage is RequestMessageEvent_Merchant_Order)批量订单的MsgId可能会相同
+                         )
+                    {
+                        //lastMessage必定有值（除非极端小的过期时间条件下，几乎不可能发生）
+                        var lastMessage = CurrentMessageContext.RequestMessages[CurrentMessageContext.RequestMessages.Count - 1];
+
+                        if (
+                            //使用MsgId去重
+                            (lastMessage.MsgId != 0 && lastMessage.MsgId == RequestMessage.MsgId) ||
+                            //使用CreateTime去重（OpenId对象已经是同一个）
+                            (lastMessage.MsgId == RequestMessage.MsgId &&
+                                 lastMessage.CreateTime == RequestMessage.CreateTime &&
+                                 lastMessage.MsgType == RequestMessage.MsgType)
+                            )
+                        {
+                            MarkRepeatedMessage();//标记为已重复
+                        }
+                    }
+
+                    #endregion
+
+                    //在消息没有被去重的情况下记录上下文
+                    if (!MessageIsRepeated)
+                    {
+                        GlobalMessageContext.InsertMessage(RequestMessage);
+                    }
+                }
+            }
+
+            return decryptDoc;
         }
     }
 }
