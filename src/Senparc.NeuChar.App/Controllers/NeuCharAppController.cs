71343894b339e245e001095f12f4b432cb8658e3
Jeffrey Su <zsu@senparc.com>
2018-11-29 18:30:39 +0800

    add NeuCharAppController

---
+++ src/Senparc.NeuChar.App/Controllers/NeuCharAppController.cs
@@ -0,0 +1,118 @@
+﻿using Senparc.NeuChar.App.MessageHandlers;
+using Senparc.NeuChar.MessageHandlers.CheckSignatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Senparc.CO2NET.HttpUtility;
+using Senparc.CO2NET.Trace;
+using System.IO;
+#if NET45
+using System.Web.Mvc;
+#else
+using Microsoft.AspNetCore.Mvc;
+#endif
+
+namespace Senparc.NeuChar.App.Controllers
+{
+    /// <summary>
+    /// 用于快速实现 NeuChar 开发者发布的 App 与 NeuChar 平台交互（如状态发送）的默认 Controller 
+    /// </summary>
+    public abstract class NeuCharAppController : Controller
+    {
+        protected abstract string Token { get; }
+
+        readonly Func<string> _getRandomFileName = () => DateTime.Now.ToString("yyyyMMdd-HHmmss") + Guid.NewGuid().ToString("n").Substring(0, 6);
+
+        /// <summary>
+        /// 后台验证地址（使用Get），微信后台的“接口配置信息”的Url填写如：http://sdk.weixin.senparc.com/weixin
+        /// </summary>
+        [HttpGet]
+        [ActionName("Index")]
+        public ActionResult Get(EncryptPostModel postModel, string echostr)
+        {
+            postModel.Token = Token;
+            if (postModel.Signature == CheckSignatureWeChat.GetSignature(postModel))
+            {
+                return Content(echostr); //返回随机字符串则表示验证通过
+            }
+            else
+            {
+                return Content("failed:" + postModel.Signature + "," + CheckSignatureWeChat.GetSignature(postModel.Timestamp, postModel.Nonce, Token) + "。" +
+                    "如果你在浏览器中看到这句话，说明此地址可以被作为 NeuChar - App后台的Url，请注意保持Token一致。");
+            }
+        }
+
+
+        /// <summary>
+        /// 最简化的处理流程（不加密）
+        /// </summary>
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult Post(EncryptPostModel postModel)
+        {
+            postModel.Token = Token;
+            if (postModel.Signature != CheckSignatureWeChat.GetSignature(postModel))
+            {
+                return Content("参数错误！");
+            }
+
+            postModel.Token = Token;
+            //postModel.EncodingAESKey = EncodingAESKey;//根据自己后台的设置保持一致
+            //postModel.AppId = AppId;//根据自己后台的设置保持一致
+
+            NeuCharAppMessageHandler messageHandler = null;
+            try
+            {
+#if NET45
+                messageHandler = new NeuCharAppMessageHandler(Request.InputStream, postModel);
+#else
+                messageHandler = new NeuCharAppMessageHandler(Request.GetRequestMemoryStream(), postModel);
+#endif
+
+                messageHandler.SaveRequestMessageLog();//记录 Request 日志（可选）
+
+                messageHandler.Execute();//执行微信处理过程（关键）
+
+                messageHandler.SaveResponseMessageLog();//记录 Response 日志（可选）
+
+                var responseText = messageHandler.TextResponseMessage ?? "";
+                return Content(responseText);
+
+            }
+            catch (Exception ex)
+            {
+                #region 异常处理
+                SenparcTrace.Log($"NeuCharAppMessageHandler错误：{ex.Message}");
+
+                var logPath = Path.Combine(messageHandler.GetLogPath(), $"Error_{_getRandomFileName()}.txt");
+                using (TextWriter tw = new StreamWriter(logPath))
+                {
+                    tw.WriteLine("ExecptionMessage:" + ex.Message);
+                    tw.WriteLine(ex.Source);
+                    tw.WriteLine(ex.StackTrace);
+                    //tw.WriteLine("InnerExecptionMessage:" + ex.InnerException.Message);
+
+                    if (messageHandler.ResponseDocument != null)
+                    {
+                        tw.WriteLine(messageHandler.ResponseDocument.ToString());
+                    }
+
+                    if (ex.InnerException != null)
+                    {
+                        tw.WriteLine("========= InnerException =========");
+                        tw.WriteLine(ex.InnerException.Message);
+                        tw.WriteLine(ex.InnerException.Source);
+                        tw.WriteLine(ex.InnerException.StackTrace);
+                    }
+
+                    tw.Flush();
+                    tw.Close();
+                }
+                return Content("");
+                #endregion
+            }
+        }
+    }
+}
