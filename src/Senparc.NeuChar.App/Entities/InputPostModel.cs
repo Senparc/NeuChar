4e8efaea68ebc0f4a6f8bcc0f5ee229727861826
FrancisFYK <fuyunkun@outlook.com>
2019-01-04 18:08:51 +0800

    增加App输入输出参数实体类型

---
+++ src/Senparc.NeuChar.App/Entities/InputPostModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Senparc.NeuChar.App.Entities
+{
+    /// <summary>
+    /// 应用中枢发送到App数据
+    /// </summary>
+    public class InputPostModel
+    {
+        /// <summary>
+        /// App 输入参数集合
+        /// </summary>
+        public List<InputParam> PostData { get; set; }
+
+        /// <summary>
+        /// 步骤编号
+        /// </summary>
+        public Guid StepId { get; set; }
+
+        /// <summary>
+        /// 应用中枢编号
+        /// </summary>
+        public int WorkflowId { get; set; }
+
+        /// <summary>
+        /// App当前配置版本号
+        /// </summary>
+        public string AppVersion { get; set; }
+    }
+}
---
+++ src/Senparc.NeuChar.App/Entities/OutputPostModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Senparc.NeuChar.App.Entities
+{
+    /// <summary>
+    /// App发送到应用中枢数据
+    /// </summary>
+    public class OutputPostModel
+    {
+        /// <summary>
+        /// App 输出参数集合
+        /// </summary>
+        public List<OutputParam> PostData { get; set; }
+
+        /// <summary>
+        /// 用户OpenId
+        /// </summary>
+        public string OpenId { get; set; }
+
+        /// <summary>
+        /// 步骤编号
+        /// </summary>
+        public Guid StepId { get; set; }
+
+        /// <summary>
+        /// 应用中枢编号
+        /// </summary>
+        public int WorkflowId { get; set; }
+
+        /// <summary>
+        /// App当前配置版本号
+        /// </summary>
+        public string AppVersion { get; set; }
+    }
+}
