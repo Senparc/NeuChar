9f4fbf11c9b80919fc360a8e92ae77a7b9830e4a
Jeffrey Su <zsu@senparc.com>
2019-05-27 21:43:20 +0800

    App v0.5.0 从 Senparc.Weixin.MP 移植引入 AppStore

---
+++ src/Senparc.NeuChar.App/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.NeuChar.App
+{
+    /// <summary>
+    /// Http 请求类型
+    /// </summary>
+    public enum HttpRequestType
+    {
+        GET,
+        POST
+    }
+}
---
+++ src/Senparc.NeuChar.App/Exceptions/NeuCharAppApiRequestException.cs
@@ -0,0 +1,39 @@
+﻿using Senparc.CO2NET.Exceptions;
+using Senparc.NeuChar.App.AppStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.NeuChar.App.Exceptions
+{
+    /// <summary>
+    /// NeuCharApp 异常
+    /// </summary>
+    public class NeuCharAppApiRequestException : NeuCharAppException
+    {
+        /// <summary>
+        /// JsonResult
+        /// </summary>
+        public ApiJsonResult JsonResult { get; set; }
+        /// <summary>
+        /// 接口 URL
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// ErrorJsonResultException
+        /// </summary>
+        /// <param name="message">异常消息</param>
+        /// <param name="inner">内部异常</param>
+        /// <param name="jsonResult">WxJsonResult</param>
+        /// <param name="url">API地址</param>
+        public NeuCharAppApiRequestException(string message, Exception inner, ApiJsonResult jsonResult, string url = null)
+            : base(message, inner)
+        {
+            JsonResult = jsonResult;
+            Url = url;
+        }
+    }
+}
