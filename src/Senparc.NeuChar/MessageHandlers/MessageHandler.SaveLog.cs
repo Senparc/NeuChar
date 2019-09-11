dcd1e65cd122601356d77e68e9603f1c52336d76
苏震巍 <zsu@senparc.com>
2019-09-12 01:18:47 +0800

    v0.7.6 优化 MessageHandler.SaveResponseMessageLog() 方法

--- src/Senparc.NeuChar/MessageHandlers/MessageHandler.SaveLog.cs
+++ src/Senparc.NeuChar/MessageHandlers/MessageHandler.SaveLog.cs
@@ -1,21 +1,41 @@
-﻿using Senparc.CO2NET.APM;
-using Senparc.CO2NET.Exceptions;
-using Senparc.CO2NET.Extensions;
-using Senparc.CO2NET.Helpers;
+﻿#region Apache License Version 2.0
+/*----------------------------------------------------------------
+
+Copyright 2018 Suzhou Senparc Network Technology Co.,Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the
+License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific language governing permissions
+and limitations under the License.
+
+Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
+
+----------------------------------------------------------------*/
+#endregion Apache License Version 2.0
+
+/*----------------------------------------------------------------
+    Copyright (C) 2018 Senparc
+    
+    文件名：MessageHandler.SaveLog.cs.cs
+    文件功能描述：微信请求日志记录
+    
+    
+    创建标识：Senparc - 20181108
+    
+    修改标识：Senparc - 20190912
+    修改描述：v0.7.6 优化 MessageHandler.SaveResponseMessageLog() 方法
+
+----------------------------------------------------------------*/
+
 using Senparc.CO2NET.MessageQueue;
-using Senparc.CO2NET.Trace;
-using Senparc.CO2NET.Utilities;
-using Senparc.NeuChar.Entities;
 using Senparc.NeuChar.Exceptions;
-using Senparc.NeuChar.NeuralSystems;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
-using System.Xml.Linq;
 #if NET35 || NET40 || NET45
 using System.Web;
 #endif
@@ -109,6 +129,13 @@ namespace Senparc.NeuChar.MessageHandlers
                         this.FinalResponseDocument.Save(Path.Combine(logPath, string.Format("{0}_Response_Final_{1}_{2}.txt", _getRandomFileName(),
                             this.ResponseMessage.ToUserName,
                             this.ResponseMessage.MsgType)));
+                    }
+
+                    if (this.ResponseDocument ==null && this.TextResponseMessage!=null)
+                    {
+                        System.IO.File.WriteAllText(Path.Combine(logPath, string.Format("{0}_TextResponse_{1}_{2}.txt", _getRandomFileName(),
+                            this.RequestMessage.ToUserName,
+                            this.RequestMessage.MsgType)));
                     }
                 });
             }
