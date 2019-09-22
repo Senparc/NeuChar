796cfd5beacb452d315610b5faf7822e81131ea2
苏震巍 <zsu@senparc.com>
2019-09-22 18:20:44 +0800

    v0.8.0.3 优化 上下文的 App 状态在缓存中的持久化更新储存问题。

--- src/Senparc.NeuChar/MessageHandlers/NeuralNode/MessageHandlerNode.Response.cs
+++ src/Senparc.NeuChar/MessageHandlers/NeuralNode/MessageHandlerNode.Response.cs
@@ -1,14 +1,45 @@
-﻿using Senparc.NeuChar.ApiHandlers;
+﻿#region Apache License Version 2.0
+/*----------------------------------------------------------------
+
+Copyright 2019 Suzhou Senparc Network Technology Co.,Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the
+License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific language governing permissions
+and limitations under the License.
+
+Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
+
+----------------------------------------------------------------*/
+#endregion Apache License Version 2.0
+
+/*----------------------------------------------------------------
+    Copyright (C) 2019 Senparc
+    
+    文件名：MessageHandlerNode.Response.cs
+    文件功能描述：MessageHandlerNode 部分类，返回消息部分的代码
+    
+    
+    创建标识：Senparc - 20181031
+
+----------------------------------------------------------------*/
+
+using Senparc.NeuChar.ApiHandlers;
 using Senparc.NeuChar.Entities;
 using Senparc.NeuChar.Helpers;
 using Senparc.NeuChar.MessageHandlers;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Senparc.NeuChar.NeuralSystems
 {
+    /// <summary>
+    /// </summary>
     public partial class MessageHandlerNode
     {
         #region 返回信息
