5e19e819131ad0b1d36e0b6dde91574ac5b9035e
苏震巍 <zsu@senparc.com>
2020-01-05 15:36:27 +0800

    创建 Senparc.NeuChar.AspNet 项目

---
+++ src/Senparc.NeuChar.AspNet/Middlewares/MessageHandlerMiddlewareExtension.cs
@@ -0,0 +1,52 @@
+﻿#region Apache License Version 2.0
+/*----------------------------------------------------------------
+
+Copyright 2019 Suzhou Senparc Network Technology Co.,Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the
+License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific language governing permissions
+and limitations under the License.
+
+Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
+
+----------------------------------------------------------------*/
+#endregion Apache License Version 2.0
+
+/*----------------------------------------------------------------
+    Copyright (C) 2019 Senparc
+    
+    文件名：MessageHandlerMiddlewareExtension.cs
+    文件功能描述：MessageHandler 中间件基类
+    
+    
+    创建标识：Senparc - 20191003
+    
+----------------------------------------------------------------*/
+
+#if NETSTANDARD2_0 || NETCOREAPP2_0 || NETCOREAPP2_1 || NETCOREAPP2_2 || NETCOREAPP3_0
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Senparc.NeuChar.Context;
+using Senparc.NeuChar.Entities;
+using Senparc.NeuChar.MessageHandlers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.NeuChar.Middlewares
+{
+    //为了方便维护，已经合并到 MessageHandlerMiddleware.cs 中
+
+}
+
+#endif
